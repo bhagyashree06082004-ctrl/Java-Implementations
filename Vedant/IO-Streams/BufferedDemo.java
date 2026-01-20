212d300ac80c10608e3dc3680bdcfa31ad97ca97
Vedant Umale <vedantumale925@gmail.com>
2026-01-20 17:39:10 +0530

    Added Java IO Streams programs

---
+++ Vedant/IO-Streams/BufferedDemo.java
@@ -0,0 +1,52 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+
+public class BufferedDemo {
+    public static void main(String[] args) throws Exception {
+        // FileInputStream fis = new FileInputStream("D://core java abdul bari//IO Streams//Test.txt");
+
+        // BufferedInputStream bis = new BufferedInputStream(fis);
+
+        //     int x;
+        //     while ((x=bis.read())!=-1) {
+        //         System.out.print((char)x);
+        //     }
+
+        //     System.out.println("file "+ fis.markSupported());
+        //     System.out.println("buffer "+ bis.markSupported());
+
+        // System.out.print((char)bis.read());
+        // System.out.print((char)bis.read());
+        // System.out.print((char)bis.read());
+        // bis.mark(3);
+
+        // System.out.print((char)bis.read());
+        // System.out.print((char)bis.read());
+        // bis.reset();
+
+        // System.out.print((char)bis.read());
+        // System.out.print((char)bis.read());
+
+
+        
+        
+        // FileReader fis = new FileReader("D://core java abdul bari//IO Streams//Test.txt");
+
+        // BufferedReader bis = new BufferedReader(fis);
+        // System.out.print((char)bis.read());
+        // System.out.print((char)bis.read());
+        // System.out.print((char)bis.read());
+        // bis.mark(3);
+
+        // System.out.print((char)bis.read());
+        // System.out.print((char)bis.read());
+        // bis.reset();
+
+        // System.out.print((char)bis.read());
+        // System.out.print((char)bis.read());
+        
+
+     }
+}
