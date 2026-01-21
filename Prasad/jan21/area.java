2967dcef5784f0518bf2192a1f8adb681f5bbc46
Pc04012004 <p1925567@gmail.com>
2026-01-21 11:36:09 +0530

    initial java prog

---
+++ Prasad/jan21/area.java
@@ -0,0 +1,13 @@
+package Prasad.jan21;
+
+public class area {
+    public static void main(String[] args) {
+        double radius = 5.0; // Example radius
+        double area = Math.PI * radius * radius; // Area = π * r²
+        
+        System.out.println("Area of Circle with radius " + radius + " is: " + area);
+    }
+}
+
+//command to run this code in terminal 
+//cd /Users/prasadchede/Desktop/Java-Implementations javac Prasad/jan21/area.java && java Prasad.jan21.area
