8c1c55cdbb57ecd1458846bacc67dc18ab6e1c83
2022bit002 <2022bit002@sggs.ac.in>
2026-01-30 12:23:04 +0530

    Java Generics

---
+++ Java Generics/GenericDemo.java
@@ -0,0 +1,15 @@
+public class GenericDemo{
+    public static void main(String[] args) {
+        Object obj = new String("Hello");
+        Object o[] = new Object[3];
+        
+        
+
+        // if we have an refernce of an object we can assign the object of any type or object of any class.
+
+        // else we need to type caste it into an particular datatype.
+
+
+
+    }
+}
